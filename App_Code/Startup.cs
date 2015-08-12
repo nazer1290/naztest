139d99dfb317f9adad18a1ef85ae0b831744af07
Matt Credland <nazer1290@gmail.com>
2015-08-12 14:42:11 +0100

    First

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(naztest.Startup))]
+namespace naztest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
